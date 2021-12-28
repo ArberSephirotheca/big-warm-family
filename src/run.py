33b0fa9b11e10e0f70b067f54735849e4a373757
Nianyu Shen <xiaoyu9964@gmail.com>
2021-12-27 21:20:16 -0500

    fix and add /comments/users

--- src/run.py
+++ src/run.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from crawler.comment import comment_crawler
+from utility.comment import Comment
 
 if __name__ == '__main__':
     app = Flask(__name__)
@@ -7,7 +7,15 @@ if __name__ == '__main__':
     @app.route('/comments')
     def comments():
         avid = request.args['avid']
-        comments = comment_crawler.get_comments_info(avid)
-        return '{}'.format(comments)
+        comment = Comment()
+        comment.get_comments_info(avid)
+        return '{}'.format(comment.comments)
+
+    @app.route('/comments/users')
+    def comments_users():
+        avid = request.args['avid']
+        comment = Comment()
+        comment.get_comments_info(avid)
+        return '{}'.format(comment.get_users())
 
     app.run(host='0.0.0.0', port=5001)
--- utility/comment.py
+++ utility/comment.py
@@ -1,39 +1,38 @@
 import requests
-import json
-
 
 comment_url = 'http://api.bilibili.com/x/v2/reply'
 
-class comment_crawler():
+
+class Comment():
+
+    def __init__(self):
+        self.comments = None
+
     def get_comments_info(self, avid):
+        replies_content = {}
         i = 1
         while True:
-            params = {
-                'type' : 1,
-                'oid' : avid,
-                'sort' : 1,
-                'nohot' : 1,
-                'pn' : i
-            }
-            response = requests.get(url = comment_url, params= params)
+            params = {'type': 1, 'oid': avid, 'sort': 1, 'nohot': 1, 'pn': i}
+            response = requests.get(url=comment_url, params=params)
             jdata = response.json()
             code = jdata['code']
             msg = jdata['message']
             data = jdata['data']
             replies = data['replies']
-            replies_content  = {}
-            if code != 0 :
+            if code != 0:
                 print("error code : ", code)
                 print(msg)
-                break
+                self.comments = replies_content
+                return
             if len(replies) == 0:
                 print("reach end of comments")
-                break
+                self.comments = replies_content
+                return
             for reply in replies:
                 user_name = reply['member']['uname']
                 comment = reply['content']['message']
                 replies_content[user_name] = comment
-            
-            print(replies_content)
-            print(i)
             i += 1
+
+    def get_users(self):
+        return self.comments.keys()
