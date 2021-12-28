287bdfa0ea76b966d7d45369cc789339d5b07887
Zheyuan Chen <czy@Zheyuans-MBP.lan>
2021-12-27 21:04:43 -0500

    change to comment crawler

--- crawler/comment.py
+++ crawler/comment.py
@@ -6,6 +6,7 @@ comment_url = 'http://api.bilibili.com/x/v2/reply'
 
 class comment_crawler():
     def get_comments_info(self, avid):
+        replies_content  = {}
         i = 1
         while True:
             params = {
@@ -21,19 +22,18 @@ class comment_crawler():
             msg = jdata['message']
             data = jdata['data']
             replies = data['replies']
-            replies_content  = {}
             if code != 0 :
                 print("error code : ", code)
                 print(msg)
-                break
+                return replies_content
+                
             if len(replies) == 0:
                 print("reach end of comments")
-                break
+                print(replies_content)
+                return replies_content
+                
             for reply in replies:
                 user_name = reply['member']['uname']
                 comment = reply['content']['message']
                 replies_content[user_name] = comment
-            
-            print(replies_content)
-            print(i)
             i += 1
