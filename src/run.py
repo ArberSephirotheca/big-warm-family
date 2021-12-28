1d6fcb0678df927a988f2b7d2883940d859ca5a2
Nianyu Shen <xiaoyu9964@gmail.com>
2021-12-27 21:38:27 -0500

    add simple html

--- src/run.py
+++ src/run.py
@@ -1,15 +1,19 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from utility.comment import Comment
 
 if __name__ == '__main__':
     app = Flask(__name__)
+
+    @app.route('/')
+    def home():
+        return render_template("index.html")
 
     @app.route('/comments')
     def comments():
         avid = request.args['avid']
         comment = Comment()
         comment.get_comments_info(avid)
-        return '{}'.format(comment.comments)
+        return render_template("index.html", comment=comment.comments)
 
     @app.route('/comments/users')
     def comments_users():
