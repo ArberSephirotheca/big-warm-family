81767c2e3802d98a5761e62980cba0d4d346909b
Zheyuan Chen <czy@Zheyuans-MBP.lan>
2021-12-30 17:03:35 -0500

    fixed minor syntax bug

--- utility/video.py
+++ utility/video.py
@@ -2,9 +2,9 @@ import requests
 
 video_url = 'http://api.bilibili.com/x/web-interface/view'
 class video_crawler():
-    def __init__(self, bvid) -> None:
-        self.bvid = bvid
+    def __init__(self, avid) -> None:
+        self.avid = avid
     
     def get_video_info(self):
-        jdata = requests.get(url= video_url, params={'bvid' : self.bvid}, timeout= 2)
+        jdata = requests.get(url= video_url, params={'avid' : self.avid}, timeout= 2)
         print(jdata.text)
