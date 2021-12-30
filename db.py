c723c4580604116b3f2b31375fb2e16fcef142eb
Zheyuan Chen <czy@Zheyuans-MBP.lan>
2021-12-30 17:32:04 -0500

    update db

--- db.py
+++ db.py
@@ -1,30 +1,26 @@
 import pymongo
-import sys
-if len(sys.argv) != 3:
-    print("specify address and port")
-    sys.exit()
-    
-host = sys.argv[1]
-port = int(sys.argv[2])
-client = pymongo.MongoClient(host= host, port = port)
-print(client)
-print("connect to mongodb successfully")
 
-class db():
-    def __init__(self, client):
-        self.db = client["big_warm_family"]
+class Mongo():
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
     
+    def connect(self):
+        self.client = pymongo.MongoClient(host=self.host, port=self.port)
+        self.db = self.client['big_warm_family']
+
     def insert_comments_info(self, comments):
         col = self.db["comments"]
+        print(col)
         for key in comments:
-            col.insert({
-                "name" : key,
-                "comment" : comments[key]
+            col.insert_one({
+                'name' : key,
+                'comment' : comments[key]
             })
     
     def find_comments_info(self, user_name):
-        col = self.db["comments"]
+        col = self.db['comments']
         found_comments = []
-        for comment in col.find({"name" : user_name}):
+        for comment in col.find_one({'name' : user_name}):
             found_comments.append(comment)
         return found_comments
--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import utility.video
-import utility.comment
+import db
+from utility.comment import Comment
 
-c = utility.comment.comment_crawler()
+c = Comment()
 c.get_comments_info(334001342)
+family = db.Mongo("127.0.0.1", 25052)
+family.connect()
+family.insert_comments_info(c.comments)
+comments = family.find_comments_info('东北爱乐人')
+for comment in comments:
+    print(comment)
