ef253f7fbde594ec31129b9b59a8be79affac60f
Nianyu Shen <xiaoyu9964@gmail.com>
2022-01-06 15:24:36 -0500

    change vite to vue-cli

---
+++ src/client/src/components/abc.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>{{ msg }}</p>
+    <button v-on:click="reverseMessage">Reverse Message</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'abc',
+  data() {
+    return {
+      msg: 'Hello!',
+    };
+  },
+  methods: {
+    reverseMessage() {
+      this.msg = 'fasdfsdf';
+    },
+  },
+};
+</script>
