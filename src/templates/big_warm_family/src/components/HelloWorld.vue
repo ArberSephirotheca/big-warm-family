8727d175d58d0b817a77a4322b1aa07af1cdd7d7
xiangtianheng <tix221@terpmail.umd.edu>
2022-01-06 14:27:43 -0500

    1# try on Vue

---
+++ src/templates/big_warm_family/src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <!--
+  <p>
+    Recommended IDE setup:
+    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
+    +
+    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+  </p>
+
+  <p>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank">
+      Vite Documentation
+    </a>
+    |
+    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+  </p>
+ -->
+<div id="v-model-basic" class="demo">
+  <p>avid: <input v-model="message" placeholder="" /> </p>
+</div>
+
+  <button type="button" @click="count++">submit</button>
+  <button type="button" @click="count++">clear</button>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
